0bf8457d62c4b3779989a14948db7646b6dfbee3
rinasvideo <rinasvideo@yahoo.co.jp>
2020-06-13 13:56:21 +0900

    add source

--- source/imap-mail.py
+++ source/imap-mail.py
@@ -8,6 +8,7 @@ import os
 import glob
 from email.header import decode_header, make_header
 import copy
+import sys
 os.system('cls')
 print('')
 
@@ -27,7 +28,7 @@ while ccd==1:
         print('')
         print(' ファイルインデックスを入力してください \n インデックスは必ず0から始まります')
         print('')
-        files=glob.glob(".\\*.bin")        
+        files=glob.glob(".\\*.bins")        
         for x in files:
             co=co+1
             print(' '+str(co)+":"+x)
@@ -38,15 +39,34 @@ while ccd==1:
         print('')
         print(' -2:手動入力')
         print('')
+        print(' ここにbinsファイルが表示されない場合は「-2」を入力してください')
+        print('')
         ac=input(' ファイルインデックス  0～ >> ')
         ac=int(ac)
         if ac==-2:
             os.system('cls')
+            print('')
             host = input(' ホスト名 >> ')
+            print('')
+            print(' このソフトウェアのセッションファイル自体はmail2.exeと互換性がありません')
+            print('')
+            file=input(' セッションファイル名 >> ')
+            file=file+".bins"
+            print('')
+            password = 'none'
+            print(' ')
+            cgn=""
+            host2="none"
+            port="none"
+            usear=[host,password,cgn,host2,port]
+            f=open(file,'wb')
+            pickle.dump(usear,f)
+            f.close()
             ccff=1
         if coun<ac:
             ac=copy.copy(coun)
         if ccff==0:
+            
             file=copy.copy(files[ac])
             g=os.path.isfile(file)
             if g==True:
@@ -54,31 +74,80 @@ while ccd==1:
                 f=open(file,'rb')
                 usear=pickle.load(f)
                 host=copy.copy(usear[0])
+                if host=="none":
+                    print('')
+                    print(' 自動ロードに失敗しました...')
+                    print('')
+                    print(' セッションファイルが対応していません')
+                    host = input(' ホスト名 >> ')
+            
         break
 
+while ccd==1:
+    try:
+        nego_combo = ("ssl", 993) # ("通信方式", port番号)
+        if nego_combo[0] == "no-encrypt":
+            imapclient = imaplib.IMAP4(host, nego_combo[1])
+        elif nego_combo[0] == "starttls":
+            context = ssl.create_default_context()
+            imapclient = imaplib.IMAP4(host, nego_combo[1])
+            imapclient.starttls(ssl_context=context)
+        elif nego_combo[0] == "ssl":
+            context = ssl.create_default_context()
+            imapclient = imaplib.IMAP4_SSL(host, nego_combo[1], ssl_context=context)
+        imapclient.debug = 0  # 各命令をトレースする
+    except:
+        print(' 接続に失敗しました...')
+        print('')
+        print(' エラー：セッションファイル名 '+file)
+        print('')
+        print(' セッション情報を設定しなおさない場合ソフトウェアを終了します')
+        print('')
+        kg=input(' 手動で接続しますか? (y or n) >>')
+        if kg=="y" or kg=="Y":
+            host = input(' ホスト名 >> ')
+           
+            print('')
+            password = 'none'
+            print(' ')
+            print(' 記憶したセッション情報をクリアするには\n アプリケーションディレクトリ内の「profile.bin」を削除してください')
+            print('')
+            cgn=input(' 次回からユーザー情報の入力を省略します (Enter) >>')
+            host2=copy.copy(usear[3])
+            port=copy.copy(usear[4])
+            usear=[host,password,cgn,host2,port]
+            f=open(file,'wb')
+            pickle.dump(usear,f)
+            f.close()
+            continue
+    else:
+        break
+while ccd==1:    
+    os.system('cls')
+    print('')        
+    print(' ログイン')
+    print('')
+    username = input(' ユーザーネーム(メールアドレス) >> ')
+    print('')
+    password = getpass.getpass(" パスワード >> ")
 
-nego_combo = ("ssl", 993) # ("通信方式", port番号)
-
-if nego_combo[0] == "no-encrypt":
-    imapclient = imaplib.IMAP4(host, nego_combo[1])
-elif nego_combo[0] == "starttls":
-    context = ssl.create_default_context()
-    imapclient = imaplib.IMAP4(host, nego_combo[1])
-    imapclient.starttls(ssl_context=context)
-elif nego_combo[0] == "ssl":
-    context = ssl.create_default_context()
-    imapclient = imaplib.IMAP4_SSL(host, nego_combo[1], ssl_context=context)
-imapclient.debug = 0  # 各命令をトレースする
-print('')        
-print(' ログイン')
+    try:
+        imapclient.login(username, password)
+    except:
+            print('')
+            print(' エラー:認証に失敗しました')
+            print('')
+            print(' ユーザーアカウントを確認してください')
+            print('')
+            print(' アカウントの権限を確認してください')
+            print('')
+            input(' リトライするにはエンターキーを押してください')
+            g=0
+            os.system('cls')
+            continue
+    else:
+        break
 print('')
-username = input(' ユーザーネーム(メールアドレス) >> ')
-print('')
-password = getpass.getpass(" パスワード >> ")
-imapclient.login(username, password)
-
-
-
 imapclient.select() # メールボックスの選択
 typ, data = imapclient.search(None, "ALL")  # data = [b"1 2 3 4 ..."]
 datas = data[0].split()
@@ -96,6 +165,7 @@ imapclient.logout()
 
 
 os.system('cls')
+c=0
 for msg in msg_list:
     print(msg)
 
